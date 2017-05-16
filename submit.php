f886caf6fcd72411afe83b26618f614836d4a2d1
JDivyom <jdivyom@gmail.com>
2017-05-16 17:22:54 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit']))
   $dbconnection = mysqli_connect('localhost','root', '','firmstep');
   if ($dbconnection)
   {
-  	echo "connection opened."
+  	echo "connection opened.";
   	$tupple = $_POST['queue'];
     $query=INSERT INTO service_que (Id, Type, Name, Service_Name, Queued_Time) VALUES ($tupple.id,$tupple.typeOfClient,$tupple.service,$tupple.name,$tupple.time_stamp);
     	$success = mysqli_query($dbconnection,$query);
