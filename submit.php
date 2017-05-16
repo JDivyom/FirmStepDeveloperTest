4343ce755e52a00cc06c67ad81b00576e12857aa
JDivyom <jdivyom@gmail.com>
2017-05-16 02:46:44 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,9 +1,21 @@
 <?php
+
 if (isset($_POST['submit']))
 {
+  $dbconnection = mysqli_connect('localhost','root', '','firmstep');
+  if ($dbconnection)
+  {
+  	echo "connection opened."
+  	$tupple = $_POST['queue'];
+    $query=INSERT INTO service_que (Id, Type, Name, Service_Name, Queued_Time) VALUES ($tupple.id,$tupple.typeOfClient,$tupple.service,$tupple.name,$tupple.time_stamp);
+    	$success = mysqli_query($dbconnection,$query);
+    	if (!$success)
+    	{
+    		die('Can not insert record into the table', mysqli_error());
+    	}
 
-echo "hello I got the Value";
-
-}
-
-?>
+  }
+  else
+  {
+  	die('error connecting to mySQL');
+  }
