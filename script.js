f663437ad7f2a0a33e2c45fe9564b8aff9c674cd
JDivyom <jdivyom@gmail.com>
2017-05-16 22:35:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -76,9 +76,10 @@ $(document).ready(function(){
 				  // $queueList.append( newQueue.id,newQueue.typeOfClient,newQueue.name,newQueue.service,newQueue.time_stamp);
 			},
 
-			error:function(XMLHttpRequest, exception, newQueue) {
-       			  alert(XMLHttpRequest.readyState);
-       			  alert( newQueue.id,newQueue.typeOfClient,newQueue.name,newQueue.service,newQueue.time_stamp);}
+			error:function(XMLHttpRequest, exception) {
+       			      alert(XMLHttpRequest.readyState);}
+			      function(newQueue){
+       			      alert( newQueue.id+newQueue.typeOfClient+newQueue.name+newQueue.service+newQueue.time_stamp);}
 
        
 
